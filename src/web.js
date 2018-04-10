3cc8d696eea1ef0c2c839a29bc19d02cd0ea1c28
OJIMA Ryoji <rojima1@gmail.com>
2018-04-10 17:54:49 +0900

    refactored.

--- src/web.js
+++ src/web.js
@@ -8,7 +8,7 @@ const ui = new UI();
 (async () => {
   const socket = new WebSocket('ws://localhost:8080/');
 
-  const winnerPromise = await (async function _(state) {
+  await (async function _(state) {
     const message = await new Promise(resolve => socket.addEventListener('message', resolve, {once: true}));
 
     const lastMove = JSON.parse(message.data).lastMove;  // 状態は自前で管理するので、lastMove以外は捨てちゃいます。
