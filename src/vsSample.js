f40160b31dd4b53570fd7409aed9a9e14be20da1
OJIMA Ryoji <rojima1@gmail.com>
2018-05-08 13:23:40 +0900

    some bugs are fixed.

--- src/vsSample.js
+++ src/vsSample.js
@@ -1,8 +1,9 @@
 import 'babel-polyfill';
 
-import {Player, State} from './game';
-import {getMove}       from './sampleAi';
-import {UI}            from './ui';
+import {count, filter, first} from 'lajure';
+import {Player, State}        from './game';
+import {getMove}              from './sampleAi';
+import {UI}                   from './ui';
 
 (async () => {
   // Chromeで画像の読み込みに時間がかかってしまう場合があったので、ダーティ・ハック……。
@@ -11,8 +12,11 @@ import {UI}            from './ui';
   document.body.style.cursor = 'wait';
 
   const ui = new UI();
+  const pastStates = [];
 
   const winner = await (async function _(state1) {
+    pastStates.push(state1);
+
     document.body.style.cursor = 'auto';
     const move1 = await ui.getMove(Array.from(state1.getLegalMoves()));
     document.body.style.cursor = 'wait';
@@ -21,6 +25,9 @@ import {UI}            from './ui';
     await ui.doMove(move1);
 
     const state2 = state1.doMove(move1);
+    if (count(filter(pastState => pastState.equals(state2), pastStates)) >= 3) {
+      return null;
+    }
     if (state2.winner) {
       return state2.winner;
     }
@@ -31,6 +38,9 @@ import {UI}            from './ui';
     await ui.doMove(move2);
 
     const state3 = state2.doMove(move2);
+    if (count(filter(pastState => pastState.equals(state3), pastStates)) >= 3) {
+      return null;
+    }
     if (state3.winner) {
       return state3.winner;
     }
